f9478be5beef4630c9b7727e74cd29785ab87c0c
JamesK712 <hjaime712@hotmail.com>
2023-08-25 23:27:02 -0500

    Fix mobile

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
     $(".p1").hover(function () {
-        arriba = Math.random() * (400 - 1) + 1;
-        abajo = Math.random() * (609 - 1) + 1;
+        document.getElementById("no").style.position = "absolute";
+        const anchoViewport = window.innerWidth;
+        const altoViewport = window.innerHeight;
+        abajo = Math.random() * (anchoViewport - 46) + 1;
+        arriba = Math.random() * (altoViewport - 46) + 1;
         $(this).css("top", arriba);
         $(this).css("left", abajo);
     });
 });
+
 function dijoSi() {
-    document.getElementById("si").style.display = "block";
+    document.getElementById("si").style.display = "flex";
+    const elementosP1 = document.getElementsByClassName("p1");
+
+    // Iterar a través de la colección y establecer la propiedad "position" para cada elemento
+    for (let i = 0; i < elementosP1.length; i++) {
+        elementosP1[i].style.position = "relative";
+    }
 }
